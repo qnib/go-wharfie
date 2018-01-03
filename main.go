1003694d3c839708d02efa38ab2ab79232f2ec24
Christian Kniep <christian@qnib.org>
2018-01-03 16:26:27 +0100

    Bump version: 0.1.1 → 0.1.2

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "CLI to help mpirun to use docker container."
 	app.Usage = "go-wharfie [options]"
-	app.Version = "0.1.1"
+	app.Version = "0.1.2"
 	app.Flags = []cli.Flag{
 		debugFlag,
 		dockerSocketFlag,
